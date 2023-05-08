2987744b0188a6aa1ac69fe00737045fde9be789
JoseGonzalez098 <gon20335@uvg.edu.gt>
2023-05-07 19:42:23 -0600

    fix

--- frozenLake.py
+++ frozenLake.py
@@ -3,85 +3,81 @@ import gymnasium as gymnasium
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 import time
 
-# Hyperparameters
-alpha = 0.1
-gamma = 0.99
-epsilon = 1.0
-min_epsilon = 0.01
-epsilon_decay = 0.999
-training_episodes = 20000
-testing_episodes = 300
 
-# Creating the Frozen Lake environment
-desc = generate_random_map(size=4)
-env = gymnasium.make("FrozenLake-v1", render_mode="human",
-                     desc=desc, is_slippery=True)
-env.metadata["render_fps"] = 30
+def main():
 
-# Q-table initialization
-q_table = np.zeros((env.observation_space.n, env.action_space.n))
+    epsilon = 1.0
+    trainCicle = 10000
+    testCicle = 300
 
-# Training the agent
-for episode in range(training_episodes):
-    state = env.reset()[0]
-    done = False
+    frozenLakeGame = gymnasium.make("FrozenLake-v1", render_mode="human",
+                                    desc=generate_random_map(size=4), is_slippery=True)
+    frozenLakeGame.metadata["render_fps"] = 30
 
-    while not done:
-        if np.random.uniform(0, 1) < epsilon:
-            action = env.action_space.sample()
-        else:
-            action = np.argmax(q_table[state, :])
+    q_table = np.zeros((frozenLakeGame.observation_space.n,
+                        frozenLakeGame.action_space.n))
 
-        next_state, reward, done, _, _ = env.step(action)
+    for i in range(trainCicle):
+        state = frozenLakeGame.reset()[0]
+        done = False
 
-        # Update the modified_reward calculation
-        if done and reward == 0:
-            modified_reward = -0.5
-        else:
-            modified_reward = reward - 0.01
-
-        q_table[state, action] += alpha * (
-            modified_reward
-            + gamma * np.max(q_table[next_state, :])
-            - q_table[state, action]
-        )
-        state = next_state
-
-    epsilon = max(min_epsilon, epsilon * epsilon_decay)  # Decay epsilon
-
-    print(f"Training progress: Episode {episode + 1} of {training_episodes}")
-
-# Testing the trained agent
-wins = 0
-iterationInfo = []
-cantIterations = 0
-
-for episode in range(testing_episodes):
-    print(f"Iteration no. {episode + 1}")
-    cantIterations += 1
-
-    state = env.reset()[0]
-    env.render()
-    done = False
-
-    while not done:
-        action = np.argmax(q_table[state, :])
-        next_state, reward, done, _, _ = env.step(action)
-        state = next_state
-        env.render()
-        time.sleep(0.5)
-
-        if done:
-            if reward == 1:
-                print("Win\n")
-                wins += 1
-                iterationInfo.append(cantIterations)
-                cantIterations = 0
+        while not done:
+            if np.random.uniform(0, 1) < epsilon:
+                action = frozenLakeGame.action_space.sample()
             else:
-                print("Game Over\n")
+                action = np.argmax(q_table[state, :])
 
-print(f"Number of wins: {wins}")
-for x in range(len(iterationInfo)):
-    print(f"{x + 1}: {iterationInfo[x]}")
+            next_state, reward, done, _, _ = frozenLakeGame.step(action)
 
-env.close()
+            if done and reward == 0:
+                modified_reward = -0.5
+            else:
+                modified_reward = reward - 0.01
+
+            q_table[state, action] += 0.1 * (
+                modified_reward
+                + 0.99 * np.max(q_table[next_state, :])
+                - q_table[state, action]
+            )
+            state = next_state
+
+        epsilon = max(0.01, epsilon * 1.0)
+
+        print("Cicle: ", i + 1)
+
+    wins = 0
+    iterationInfo = []
+    cantIterations = 0
+
+    for i in range(testCicle):
+        print(f"Iteration no. {i + 1}")
+        cantIterations += 1
+
+        state = frozenLakeGame.reset()[0]
+        frozenLakeGame.render()
+        done = False
+
+        while not done:
+            action = np.argmax(q_table[state, :])
+            next_state, reward, done, _, _ = frozenLakeGame.step(action)
+            state = next_state
+            frozenLakeGame.render()
+            time.sleep(0.5)
+
+            if done:
+                if reward == 1:
+                    print("Agente logro el objetivo + 1")
+                    wins += 1
+                    iterationInfo.append(cantIterations)
+                    cantIterations = 0
+                else:
+                    print("Agente no logro el objetivo")
+
+    print(f"RESULTADOS: ", wins)
+    for x in range(len(iterationInfo)):
+        print(f"{x + 1}: {iterationInfo[x]}")
+
+    frozenLakeGame.close()
+
+
+main()
